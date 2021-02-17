5b568705410fa6a656a0c00732c535087d32dbc4
7Inc Software <76611131+7incsoft@users.noreply.github.com>
2021-02-17 12:07:42 +0700

    Update ryubot-panel.php

--- ryubot-panel.php
+++ ryubot-panel.php
@@ -1,19 +1,52 @@
-?><!DOCTYPE html>
+?>
+<?php
+$base = __DIR__.'/ryulogs';
+$scan = scandir($base);
+?>
+<!DOCTYPE html>
 <html>
 <head>
 	<title>:: RyuBot Panel ::</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
 	<style type="text/css">
-		html,body{background: #eee;color: #444}
-		table{width: 60%;margin:  0 auto;}
-		table, th, td {border: 1px solid black;border-collapse:collapse;text-align: center;}
-	th{background: #000;color: #eee;text-align: center;}
-	a{color: #000;text-decoration: none;font-style: italic;}
+		html,body{scroll-behavior: smooth;}
+		.section{min-height: 700px;height: 100%;margin-top: 100px}
+		.navbar-float{position:fixed;left: 0;margin-top: 20%}
+		.navbar-float>li{list-style: none;margin: 10px}
+		.code{background: #333}.mt-2{margin-top: 20px}
 	</style>
+	<link rel="stylesheet" type="text/css" href="https://brobin.github.io/hacker-bootstrap/css/hacker.css">
 </head>
 <body>
+	<div class="navbar-float">
+	<li><a href="#welcome" class="btn btn-danger">&#x2680;</a></li>
+	<li><a href="#statistic" class="btn btn-warning">&#x2681;</a></li>
+	<li><a href="#config" class="btn btn-primary">&#x2682;</a></li>
+</div>
+	<div class="container container-fluid" style="width: 70%">
+	
+<div class="section" id="welcome">
 <center><h1>RyuBot Panel</h1></center>
 <hr>
-<table >
+<div class="row">
+	<?php
+	foreach($scan as $dir){
+		if(!preg_match("/log/",$dir))continue;
+		?>
+
+	<div class="col-md-6 col-xs-12 col-sm-12 col-lg-6 mt-2">
+		<h3><?=$dir;?></h3>
+		<textarea class="form-control" style="width: 100%;height: 300px"><?=@file_get_contents($base.'/'.$dir);?></textarea>
+	</div>
+	<?php
+}
+?>
+</div>
+</div>
+<div id="statistic" class="section">
+<div class="table-responsive">
+<table class="table table-hover table-striped">
 	<thead>
 		<th>No.</th>
 		<th>Filename</th>
@@ -24,8 +57,7 @@
 	<tbody>
 	</tbody>
 	<?php
-	$base = __DIR__.'/ryulogs';
-	$scan = scandir($base);
+	
 	$n=1;
 	foreach($scan as $dir){
 		if($dir == '.' || $dir =='..' )continue;
@@ -35,12 +67,26 @@
 		<td><?=$dir;?></td>
 		<td><?=$this->human_filesize(filesize($base.'/'.$dir));?></td>
 		<td><?=$this->count_line($base.'/'.$dir);?></td>
-		<td><a href="?ryupanel=delete&file=ryulogs/<?=$dir;?>">Delete</a></td>
+		<td>
+			<a href="ryulogs/<?=$dir;?>">View</a>|<a href="?ryupanel=delete&file=ryulogs/<?=$dir;?>">Delete</a></td>
+	</tr>
 	<?php
 	}
 	?>
 	</tbody>
 </table>
+</div>
+
+</div>
+<div id="config" class="section">
+	<pre class="code">
+	<?php
+
+	print_r(json_encode($this->config,JSON_PRETTY_PRINT));
+	?>
+</pre>
+</div>
+</div>
 </body>
 </html>
 			<?php
